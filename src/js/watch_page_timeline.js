7cd22c3e8ca62e473048d0d6ae5aa2a316d68b3b
sdevpolaris <sdevpolaris@gmail.com>
2016-09-09 21:51:29 -0400

    Removed old files. Preparing for a new feature

---
+++ src/js/watch_page_timeline.js
@@ -0,0 +1,10 @@
+(function(){
+
+  'use strict';
+
+  // className for the preview tooltip that popups when you hover the progress bar:
+  // ytp-tooltip ytp-bottom ytp-preview, the previews
+
+  var padding = document.getElementsByClassName('ytp-progress-bar-container')[0];
+
+})();
