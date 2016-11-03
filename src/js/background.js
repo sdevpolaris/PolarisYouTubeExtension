7d95de29c09ee5d0d6e82ca9cd4110c8d3675645
sdevpolaris <sdevpolaris@gmail.com>
2016-11-02 23:50:13 -0400

    Added all the current features to options page. Added loading of default settings upon first installation

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,32 @@
 (function(){
+
+  'use strict';
+
+  function setInitialSettings() {
+    var defaultSettings = {
+      YT_SUBFILTER: true,
+      YT_PLAYER_CUSTOM_CONTROLS : true,
+      YT_WATCH_PAGE_SEARCH : true,
+      YT_WATCH_PAGE_SHOW_LIKE_PERCENTAGE : true,
+      YT_WATCH_PAGE_HIDE_RECOMMEND : true,
+      YT_WATCH_PAGE_PUBLISH_TIME_DAYS : true,
+      YT_WATCH_PAGE_SHOW_HIDE_COMMENTS : true,
+      YT_PLAYER_ANNOTATIONS_OFF : true,
+      YT_HOVERCARDS_OFF : false,
+      YT_PLAYER_SHARE_ON_END_OFF : true
+    };
+
+    chrome.storage.sync.set({
+      polaris : defaultSettings
+    }, function() {
+      console.log('Default settings loaded');
+    });
+  }
+
+  chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason === 'install') {
+      setInitialSettings();
+    }
+  });
+
 })();
--- src/js/options.js
+++ src/js/options.js
@@ -1,5 +1,53 @@
 (function() {
 
   'use strict';
+
+  function loadSavedSetting(callback) {
+    chrome.storage.sync.get('polaris', function(items) {
+      callback(items.polaris);
+    });
+  }
+
+  function initializeTogglesWithSettings(settings) {
+    for (var id in settings) {
+      var enabled = settings[id];
+      if (enabled) {
+        var checkbox = $('#' + id);
+        checkbox.prop('checked', true);
+      }
+    }
+  }
+
+  function toggleChanged() {
+    console.log('set dirty');
+  }
+
+  function initializeTogglesWithListeners(settings) {
+    for (var id in settings) {
+      var checkbox = $('#' + id);
+      checkbox.change(toggleChanged);
+    }
+  }
+
+  function initializeFunctions(settings) {
+    initializeTogglesWithSettings(settings);
+    initializeTogglesWithListeners(settings);
+    // saveChanges(settings);
+  }
+
+  function saveChanges(settings) {
+    for (var id in settings) {
+      var checkbox = $('#' + id);
+      settings[id] = checkbox[0].checked;
+    }
+    chrome.storage.sync.set({
+      polaris : settings
+    }, function() {
+      console.log('Saved settings');
+    });
+  }
+
+  loadSavedSetting(initializeFunctions);
+
   
 })();
