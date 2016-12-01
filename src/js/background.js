b2c00722514cdcf0a77d02259aa246f5bf6f7163
sdevpolaris <sdevpolaris@gmail.com>
2016-12-01 14:57:15 -0500

    Added feature to hide video/channel suggestion overlays in a video

--- src/js/background.js
+++ src/js/background.js
@@ -18,7 +18,8 @@
     YT_PLAYER_CUSTOM_CONTROLS          : { enable : true , inject: false },
     YT_PLAYER_ANNOTATIONS_OFF          : { enable : true , inject: true  },
     YT_PLAYER_SHOW_TOP_INFO            : { enable : false, inject: true  },
-    YT_PLAYER_HIDE_END_SCREEN          : { enable : true , inject: false }
+    YT_PLAYER_HIDE_END_SCREEN          : { enable : true , inject: false },
+    YT_PLAYER_HIDE_SUGGESTION_OVERLAYS : { enable : true , inject: false }
   };
 
   function loadSavedSetting(callback) {
--- src/js/player_general.js
+++ src/js/player_general.js
@@ -2,6 +2,11 @@
 
   'use strict';
 
+  // Each of the functions in this encapsulated self-invoking function has to keep its
+  // own set of variables that reference existing dom elements on the document (such as the movie_player)
+  // such that mutation observers will not observe on older variables (due to YouTube's spf), and GC will handle
+  // unused variables from previous function calls
+
   function hidePlayerEndScreen() {
     var player = document.getElementById('movie_player');
 
@@ -34,6 +39,48 @@
     observer.observe(player, config);
   }
 
+  function hideSuggestionOverlays() {
+    var player = document.getElementById('movie_player');
+
+    // These overlays can either be loaded at the start of video, or during the video
+    // Need an observer for the second case
+
+    var overlays = player.getElementsByClassName('ytp-ce-element');
+    if (overlays.length > 0) {
+      for (var i = 0; i < overlays.length; i++) {
+        overlays[i].classList.add('ce-hide');
+      }
+    } else {
+      var observer = new MutationObserver(function(mutations) {
+        for (var index in mutations) {
+          var mutation = mutations[index];
+
+          // Check for the mutations that add HTML dom nodes
+
+          if (mutation.addedNodes.length > 0
+            && typeof mutation.addedNodes[0] === 'object'
+            && mutation.addedNodes[0].nodeType === 1
+            && mutation.addedNodes[0].classList.contains('ytp-ce-element')) {
+
+            for (var i = 0; i < mutation.addedNodes.length; i++) {
+              mutation.addedNodes[i].classList.add('ce-hide');
+            }
+
+            // Continue the loop since the overlays will be added in sequence, and we want to disconnect
+            // the observer as soon as possible
+            continue;
+          }
+          // Disconnect the observer immediately once all consecutive ce-elements are loaded
+          observer.disconnect();
+        }
+      });
+
+      var config = { attributes: false, subtree: true, childList: true };
+      observer.observe(player, config);
+    }
+  }
+
   polarisYT['YT_PLAYER_HIDE_END_SCREEN'] = { action: hidePlayerEndScreen };
+  polarisYT['YT_PLAYER_HIDE_SUGGESTION_OVERLAYS'] = { action : hideSuggestionOverlays };
 
 })();
